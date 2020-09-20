070e5661bde74f2f8202ed3c63fd9bebd80dec56
endieposts <13526487+endieposts@users.noreply.github.com>
2020-09-20 14:53:27 +0100

    Add keycloak settings; fix filename typo; change server port

--- src/main/java/com/endie/avizandum/maintenance/controllers/WebController.java
+++ src/main/java/com/endie/avizandum/maintenance/controllers/WebController.java
@@ -1,7 +1,18 @@
 package com.endie.avizandum.maintenance.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
+
+    @GetMapping(path = "/")
+    public String index() {
+        return "unverified";
+    }
+
+    @GetMapping(path = "/verified")
+    public String verified() {
+        return "verified";
+    }
 }
